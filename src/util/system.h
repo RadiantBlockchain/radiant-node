02f9523f9a56dea1b8bdefd2d6cff4ff61af9e36
Wladimir J. van der Laan <laanwj@gmail.com>
2018-11-21 16:43:46 +0100

    [backport#14708] Warn unrecognised sections in the config file
    Summary:
    3fb09b9889665a24b34f25e9d1385a05058a28b7 Warn unrecognized sections in the config file (Akio Nakamura)
    
    Pull request description:
    
      This PR intends to resolve #14702.
    
      In the config file, sections are specified by square bracket pair "[]"$,
      or included in the option name itself which separated by a period"(.)".
    
      Typicaly, [testnet] is not a correct section name and specified options
      in that section are ignored but user cannot recognize what is happen.
    
      So, add some log-warning messages if unrecognized section names are
      present in the config file after checking section only args.
    
      note: Currentry, followings are out of scope of this PR.
      1) Empty section name or option name can describe.
      e.g. [] , .a=b, =c
      2) Multiple period characters can exist in the section name and option name.
      e.g. [c.d.e], [..], f.g.h.i=j, ..=k
    
    Tree-SHA512: 2cea02a0525feb40320613989a75cd7b7b1bd12158d5e6f3174ca77e6a25bb84425dd8812f62483df9fc482045c7b5402d69bc714430518b1847d055a2dc304b
    
    ---
    
    This is a backport of Core [[https://github.com/bitcoin/bitcoin/pull/14708 | PR14708]]
    
    Test Plan:
      ninja
      test_runner.py feature_config_args.py
    
    Reviewers: deadalnix, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: deadalnix, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D5756

--- src/util/system.h
+++ src/util/system.h
@@ -153,6 +153,7 @@ protected:
     std::set<std::string> m_network_only_args GUARDED_BY(cs_args);
     std::map<OptionsCategory, std::map<std::string, Arg>>
         m_available_args GUARDED_BY(cs_args);
+    std::set<std::string> m_config_sections GUARDED_BY(cs_args);
 
     bool ReadConfigStream(std::istream &stream, std::string &error,
                           bool ignore_invalid_keys = false);
@@ -174,7 +175,12 @@ public:
      * in the default section but not overridden on the command line or in a
      * network-specific section in the config file.
      */
-    void WarnForSectionOnlyArgs();
+    const std::set<std::string> GetUnsuitableSectionOnlyArgs() const;
+
+    /**
+     * Log warnings for unrecognized section names in the config file.
+     */
+    const std::set<std::string> GetUnrecognizedSections() const;
 
     /**
      * Return a vector of strings of the given argument
