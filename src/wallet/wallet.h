5cdb9fe6502fc27588773e657697dbf2b2358e6b
Wladimir J. van der Laan <laanwj@gmail.com>
2019-01-27 16:11:07 +0100

    Merge #15254: Trivial: fixup a few doxygen comments
    Summary:
    70e7cee96028889ab709fa3c479225e584fcb77f Trivial: Doxygenize existing CBufferedFile and VectorReader comments (Ben Woosley)
    9431e1b91598fc255234ede10c22208ad18685cd Trivial: fixup a few doxygen comments (Ben Woosley)
    
    Pull request description:
    
      These were not declared properly, so their results are not properly
      processed. E.g.:
      https://dev.visucore.com/bitcoin/doxygen/rpcdump_8cpp.html#a994c8748aaa60fbb78009ff8a0638dea
      https://dev.visucore.com/bitcoin/doxygen/coins_8cpp.html#aa03af24ef3570144b045f4fca7a0d603
      https://dev.visucore.com/bitcoin/doxygen/wallet_2wallet_8cpp.html#a5c2a7725ff8796f03471f844ecded3d9
    
      > A third alternative is to use a block of at least two C++ comment lines, where each line starts with an additional slash or an exclamation mark.
    
      http://www.doxygen.nl/manual/docblocks.html
    
    Tree-SHA512: c13fd48ac78f25e51b1281385747e8be4bd6e27e0a0f8704608aa5c66c16715c639f1cf27018b1bf05fc2eaed6c7b9be05a68365ffe1d88dd3f400d453b7bead
    
    Backport of Core [[https://github.com/bitcoin/bitcoin/pull/15254 | PR15254]]
    
    Review notes:
    `VerifyWallets()` has already been removed.
    Changes to `wallet/rcpdump.cpp` were made in D6140.
    
    Test Plan:
      ninja
    
    Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D6475

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -61,10 +61,10 @@ void StopWallets();
 void UnloadWallets();
 
 //! Explicitly unload and delete the wallet.
-//  Blocks the current thread after signaling the unload intent so that all
-//  wallet clients release the wallet.
-//  Note that, when blocking is not required, the wallet is implicitly unloaded
-//  by the shared pointer deleter.
+//! Blocks the current thread after signaling the unload intent so that all
+//! wallet clients release the wallet.
+//! Note that, when blocking is not required, the wallet is implicitly unloaded
+//! by the shared pointer deleter.
 void UnloadWallet(std::shared_ptr<CWallet> &&wallet);
 
 bool AddWallet(const std::shared_ptr<CWallet> &wallet);
@@ -632,8 +632,8 @@ public:
     int64_t nTimeCreated;
     int64_t nTimeExpires;
     std::string strComment;
-    //! todo: add something to note what created it (user, getnewaddress,
-    //! change) maybe should have a map<string, string> property map
+    // todo: add something to note what created it (user, getnewaddress,
+    // change) maybe should have a map<string, string> property map
 
     explicit CWalletKey(int64_t nExpires = 0);
 
