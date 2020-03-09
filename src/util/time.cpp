8e1180c3f7577cbd5055691e3271bc16f239cbf1
Fabien <fabcien@gmail.com>
2020-03-09 09:44:28 +0100

    Misuse of the Visual Studio version preprocessor macro
    Summary:
    The `gmtime_s` function (used in `src/util/time.cpp`) is a Microsoft C
    runtime (CRT) function (arguments are reversed wrt C11).
    
    There is a preprocessor switch to only use this function with Visual
    Studio, but this is inaccurate. This should be used with all the
    compilers that use the CRT, including MinGw (but not Cygwin for
    example). The correct preprocessor macro for this is `_WIN32`.
    
    Note: MinGw used to define `_MSC_VER` under certain circumstances, but
    it should not be relied upon (see discussion from
    https://sourceforge.net/p/mingw-w64/bugs/282/). This definition appears
    to be dependent on headers ordering (D5445 will break the Windows build
    as is, but won't if `chrono` is included last). As a consequence it also
    fixes the build with recent versions of MinGw (building on Debian
    testing was broken).
    
    Test Plan:
    Run the Gitian builds.
    Build for windows on Debian testing.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D5448

--- src/util/time.cpp
+++ src/util/time.cpp
@@ -66,7 +66,7 @@ void MilliSleep(int64_t n) {
 std::string FormatISO8601DateTime(int64_t nTime) {
     struct tm ts;
     time_t time_val = nTime;
-#ifdef _MSC_VER
+#ifdef _WIN32
     gmtime_s(&ts, &time_val);
 #else
     gmtime_r(&time_val, &ts);
@@ -79,7 +79,7 @@ std::string FormatISO8601DateTime(int64_t nTime) {
 std::string FormatISO8601Date(int64_t nTime) {
     struct tm ts;
     time_t time_val = nTime;
-#ifdef _MSC_VER
+#ifdef _WIN32
     gmtime_s(&ts, &time_val);
 #else
     gmtime_r(&time_val, &ts);
