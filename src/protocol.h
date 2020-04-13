7dea8089dd04edd4df5b25d0fbe2d517000ce9e2
freetrader <freetrader@tuta.io>
2020-04-13 12:49:33 +0000

    Update some remaining resolvable references to ABC to src/ and contrib/
    This touches some sources files, so the test plan covers more than just
    review.
    
    Note: src/rpc/register.h contains a comment associated with an interface
          that still has 'ABC' baked into its name. This has been left as-is
          pending a decision whether to rename this interface going forward,
          or leave the name. Author's opinion: Extending it while keeping it
          named after ABC would not be optimal.
    
    Test plan:
      - review the document / pure comment changes
      - do a full build and test with `ninja check` and `ninja check-extended`,
        all tests expected to pass.
    
    Closes #37, Related to #69

--- src/protocol.h
+++ src/protocol.h
@@ -280,16 +280,16 @@ enum ServiceFlags : uint64_t {
     // Nothing
     NODE_NONE = 0,
     // NODE_NETWORK means that the node is capable of serving the complete block
-    // chain. It is currently set by all Bitcoin ABC non pruned nodes, and is
+    // chain. It is currently set by all Bitcoin Cash Node non pruned nodes, and is
     // unset by SPV clients or other light clients.
     NODE_NETWORK = (1 << 0),
     // NODE_GETUTXO means the node is capable of responding to the getutxo
-    // protocol request. Bitcoin ABC does not support this but a patch set
+    // protocol request. Bitcoin Cash Node does not support this but a patch set
     // called Bitcoin XT does. See BIP 64 for details on how this is
     // implemented.
     NODE_GETUTXO = (1 << 1),
     // NODE_BLOOM means the node is capable and willing to handle bloom-filtered
-    // connections. Bitcoin ABC nodes used to support this by default, without
+    // connections. Bitcoin Cash Node nodes used to support this by default, without
     // advertising this bit, but no longer do as of protocol version 70011 (=
     // NO_BLOOM_VERSION)
     NODE_BLOOM = (1 << 2),
