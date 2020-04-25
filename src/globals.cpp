eab6dd1687a6c6f5a1bb79653b997d8bf41bfbf4
Calin Culianu <calin.culianu@gmail.com>
2020-04-26 02:26:29 +0300

    Removed globals.cpp and globals.h
    These files were completely empty.  What's more, globals.cpp actually produced
    warnings when linking using Xcode/clang about empty object files.

--- src/globals.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2017 Amaury SÉCHET
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
--- src/globals.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017 Amaury SÉCHET
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-#ifndef BITCOIN_GLOBALS_H
-#define BITCOIN_GLOBALS_H
-
-#endif // BITCOIN_GLOBALS_H
