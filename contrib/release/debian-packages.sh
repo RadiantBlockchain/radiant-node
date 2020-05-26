df24a4a467fad579d835ed34603ff0e628f6bb4c
Andrea Suisani <sickpig@gmail.com>
2020-05-26 17:35:07 +0200

    Adapt debian-packages.sh to work with BCHN infrastructure (take #2)
    Notable changes:
    
    - add xenial to the list of the supported Ubuntu versions
    - special cased xenial pkg build (use autotoolsr/g++8 rather than cmake/default g++)
    - add a debian version parameter, as in https://www.debian.org/doc/debian-policy/ch-controlfields.html#standards-version), to the script
    - add a way to build the pkgs for a single supported version rather than build it from all every time

--- contrib/release/debian-packages.sh
+++ contrib/release/debian-packages.sh
@@ -21,20 +21,31 @@ help_message() {
   echo
   echo "Note: This script will prompt you to sign with your PGP key."
   echo
-  echo "-d, --dry-run             Build and sign the packages, but do not push them to the PPA."
-  echo "-h, --help                Display this help message."
-  echo "-p, --ppa <ppa-name>      PPA hostname. Defaults to: '${DEFAULT_PPA}'. If no config file exists at ${DPUT_CONFIG_FILE}"
-  echo "                            then one will be created using '${DEFAULT_PPA}'. Setting this option to a hostname other than"
-  echo "                            the default will require that you add the necessary settings to the config file."
-  echo "-v, --version <version>   Set the package version. Defaults to the version returned by 'bitcoind --version'."
-  echo "                            If set, version must be of the form: MAJOR.MINOR.REVISION[.OPTIONALPATCH]"
-  echo "                            OPTIONALPATCH may be necessary when source files have changed but the version revision has not,"
-  echo "                            as the PPA will reject source archives of the same name."
+  echo "-d, --dry-run               Build and sign the packages, but do not push them to the PPA."
+  echo "-h, --help                  Display this help message."
+  echo "-p, --ppa <ppa-name>        PPA hostname. Defaults to: '${DEFAULT_PPA}'. If no config file exists at ${DPUT_CONFIG_FILE}"
+  echo "                              then one will be created using '${DEFAULT_PPA}'. Setting this option to a hostname other than"
+  echo "                              the default will require that you add the necessary settings to the config file."
+  echo "-v, --version <version>     Set the package version. Defaults to the version as stated in CMakeLists.txt."
+  echo "                              If set, version must be of the form: MAJOR.MINOR.REVISION[.OPTIONALPATCH]"
+  echo "                              OPTIONALPATCH may be necessary when source files have changed but the version revision has not,"
+  echo "                              as the PPA will reject source archives of the same name."
+  echo "-D, --deb-version <version> Set the package debian/ubuntu version. By default the value would be '1'. This version would be"
+  echo "                              appended to the upstream version and the name of the distro (as defined by the debian packaging policies"
+  echo "                              related to package versioning, for more details see:"
+  echo "                              https://www.debian.org/doc/debian-policy/ch-controlfields.html#standards-version"
+  echo "                              If set, version must be of the form of a single positive integer number."
+  echo "                              This parameter be necessary when source files have not changed but the build of the"
+  echo "                              packages failed for issues unrelated to the code base"
+  echo "-u, --ubuntu-name <name>    A string parameter that specify the name of the ubuntu version we want to build the packages for. If not used"
+  echo "                              the packages will be built for all the supported Ubuntu version. E.g. -u xenial."
 }
 
 DRY_RUN="false"
 NUM_EXPECTED_ARGUMENTS=1
 PACKAGE_VERSION=""
+DEBIAN_PKG_VERSION=""
+UBUNTU_NAME=""
 PPA="${DEFAULT_PPA}"
 
 # Parse command line arguments
@@ -59,6 +70,28 @@ case $1 in
       echo "Error: package_version is not formatted correctly"
       echo
       help_message
+      exit 20
+    }
+    shift # shift past argument
+    shift # shift past value
+    ;;
+  -D|--debian-version)
+    DEBIAN_PKG_VERSION="$2"
+    echo "${DEBIAN_PKG_VERSION}" | grep -E "[0-9]+?" || {
+      echo "Error: debian_version is not formatted correctly"
+      echo
+      help_message
+      exit 20
+    }
+    shift # shift past argument
+    shift # shift past value
+    ;;
+  -u|--ubuntu-name)
+    UBUNTU_NAME="$2"
+    SUPPORTED_VERSIONS="xenial bionic eoan focal"
+    echo "${SUPPORTED_VERSIONS}" | grep -qw ${UBUNTU_NAME} || {
+      echo "$UBUNTU_NAME is not a valid or it is an unsupported Ubuntu version"
+      echo "Supported versions are: ${SUPPORTED_VERSIONS}"
       exit 20
     }
     shift # shift past argument
@@ -129,48 +162,71 @@ grep "\[${PPA}\]" ${DPUT_CONFIG_FILE} || {
 }
 
 # Build package source archive
-"${TOPLEVEL}"/contrib/devtools/build_cmake.sh
+"${TOPLEVEL}"/contrib/release/configure_cmake.sh
 pushd "${TOPLEVEL}"/build
 ninja package_source
 
 # Get package version if one wasn't explicitly set
 if [ -z "${PACKAGE_VERSION}" ]; then
-  PACKAGE_VERSION=$(./src/bitcoind --version | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+  PACKAGE_VERSION=$(grep VERSION ../CMakeLists.txt | grep -v required | awk '{print $2}')
 fi
 echo "Package version: ${PACKAGE_VERSION}"
 
+# Get debian version if one wasn't explicitly set
+if [ -z "${DEBIAN_PKG_VERSION}" ]; then
+  DEBIAN_PKG_VERSION=1
+fi
+echo "Debian version: ${DEBIAN_PKG_VERSION}"
 # Unpack the package source
 SOURCE_VERSION=$(echo "${PACKAGE_VERSION}" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
 SOURCE_BASE_NAME="bitcoin-cash-node-${SOURCE_VERSION}"
 SOURCE_ARCHIVE="${SOURCE_BASE_NAME}.tar.gz"
+echo "tar -zxf ${SOURCE_ARCHIVE}"
 tar -zxf "${SOURCE_ARCHIVE}"
 
 # Rename the package source archive. debuild is picky about the naming.
 CONTROL_SOURCE_NAME=$(grep "Source: " "${TOPLEVEL}"/contrib/debian/control | cut -c 9-)
 PACKAGE_BASE_NAME="${CONTROL_SOURCE_NAME}_${PACKAGE_VERSION}"
 PACKAGE_ARCHIVE="${PACKAGE_BASE_NAME}.orig.tar.gz"
+echo "mv ${SOURCE_ARCHIVE}" "${PACKAGE_ARCHIVE}"
 mv "${SOURCE_ARCHIVE}" "${PACKAGE_ARCHIVE}"
 
 # Build package files for each supported distribution
 DATE=$(date -R)
 package() {
   DISTRO="$1"
-  PACKAGE_NAME="${PACKAGE_BASE_NAME}-${DISTRO}"
+  PACKAGE_NAME="${PACKAGE_BASE_NAME}-${DISTRO}${DEBIAN_PKG_VERSION}"
 
   pushd "${SOURCE_BASE_NAME}"
   cp -r "${TOPLEVEL}"/contrib/debian .
 
-# Generate the changelog for this package
-# TODO: Incorporate release notes into this changelog
+  # xenial need a g++8 to get the code compiled so we have a custom
+  # control file for that, so that we can avoid to pollute the lsit
+  # dependencies for the other supported ubuntu versions.
+  # also for xenial we used autotools rather than cmake.
+  if [ ${DISTRO} == "xenial" ]; then
+    cp debian/xenial/* debian/
+  fi
+
+  # Generate the changelog for this package
   cat > debian/changelog <<EOF
-${CONTROL_SOURCE_NAME} (${PACKAGE_VERSION}-${DISTRO}) ${DISTRO}; urgency=medium
+${CONTROL_SOURCE_NAME} (${PACKAGE_VERSION}-${DISTRO}${DEBIAN_PKG_VERSION}) ${DISTRO}; urgency=medium
 
   * New upstream release.
+
+  For an exhaustive list of changes please have a look at https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/-/blob/master/doc/release-notes.md
 
  -- ${SIGNER}  ${DATE}
 EOF
 
-  debuild -S
+  # if this is the fist try to upload we should als o need to upload
+  # the orig.tar.gz containing the source code, hence we need to use "-sa"
+  if [ $DEBIAN_PKG_VERSION -eq 1 ]; then
+    debuild -S -sa
+  else
+    debuild -S -sd
+  fi
+
   rm -rf debian
   popd
 
@@ -181,11 +237,17 @@ EOF
   fi
 }
 
-# Bionic: Ubuntu 18.04 LTS
-package "bionic"
-# Eoan: Ubuntu 19.10
-package "eoan"
-# Focal: Ubuntu 20.04 LTS
-package "focal"
+if [ -z "${UBUNTU_NAME}" ]; then
+  # Xenial: Ubuntu 16.04 LTS
+  package "xenial"
+  # Bionic: Ubuntu 18.04 LTS
+  package "bionic"
+  # Eoan: Ubuntu 19.10
+  package "eoan"
+  # Focal: Ubuntu 20.04 LTS
+  package "focal"
+else
+  package ${UBUNTU_NAME}
+fi
 
 popd
