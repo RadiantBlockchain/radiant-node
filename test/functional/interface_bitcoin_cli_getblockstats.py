1ef830ae6c57aae3c3aa2e2dcfeaecf693730b4a
freetrader <freetrader@tuta.io>
2020-06-01 16:43:03 +0000

    [tests] Add simple test of getblockstats call via bitcoin-cli
    This test should pass on current master, showing how a command that
    expects a hash_or_height argument must be called with explicit quotes
    when passing a hash argument, otherwise it would fail with a JSON
    parsing error.
    
    Test plan:
      - `test/functional/test_runner.py interface_bitcoin_cli_getblockstats.py`
    
    References: Core PR#15412, PR#15488, Bitcoin Cash Node MRs !445, !446

---
+++ test/functional/interface_bitcoin_cli_getblockstats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test bitcoin-cli getblockstats"""
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_raises_process_error,
+)
+
+
+class TestBitcoinCliGetBlockStats(BitcoinTestFramework):
+
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 1
+
+    def run_test(self):
+        """Main test logic"""
+
+        # get chain's genesis block hash, we will be using it a few times
+        block_0_hash = self.nodes[0].getblockhash(0)
+
+        # check that call using simple block height works naturally
+        self.log.info("Testing `bitcoin-cli getblockstats 0`")
+        cli_response = self.nodes[0].cli("getblockstats", 0).send_cli()
+        assert_equal(cli_response['blockhash'], block_0_hash)
+
+        # check that it works with quoted strings
+        self.log.info(
+            "Testing `bitcoin-cli getblockstats '\"{}\"'`".format(block_0_hash))
+        cli_response = self.nodes[0].cli("getblockstats", "\"{}\"".format(block_0_hash)).send_cli()
+        assert_equal(cli_response['blockhash'], block_0_hash)
+
+        # check that is raises JSON parsing error without the quotes
+        assert_raises_process_error(
+            1, "Error parsing JSON:{}".format(block_0_hash),
+            self.nodes[0].cli("getblockstats", "{}".format(block_0_hash)).echo)
+
+
+if __name__ == '__main__':
+    TestBitcoinCliGetBlockStats().main()
