504a3c1dff88d0d6117fea24cf7e57a68385cae0
Søren Bredlund Caspersen <soeren.b.c@gmail.com>
2020-06-06 00:55:56 +0000

    Remove trailing spaces from generated Markdown documentation
    This catches all the trailing spaces raised in https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/-/issues/118 .
    
    Most fixed through the `HelpExampleCli` function in `src/rpc/server.cpp`, the rest by deleting spaces in the various `*.cpp` files
    
    Test plan:
    - review code changes
    - build then and test: `mkdir build && cd build && cmake -GNinja .. && ninja check
    - generate the documentation files
      `cd .. && BUILDDIR=$PWD/build contrib/devtools/gen-manpages.sh`
    - check that the generated files contain no trailing whitespace:
      `find doc/cli doc/man doc/json-rpc -name \*.md -o -name \*.1 | while read docfile; do grep -q '[[:space:]]$' $docfile && echo "$docfile contains trailing space"; done`
    
    Check that the last command produces no "contains trailing space" output.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -588,7 +588,11 @@ std::vector<std::string> CRPCTable::listCommands() const {
 
 std::string HelpExampleCli(const std::string &methodname,
                            const std::string &args) {
-    return "> bitcoin-cli " + methodname + " " + args + "\n";
+    if (args.empty()) {
+        return "> bitcoin-cli " + methodname + "\n";
+    } else {
+        return "> bitcoin-cli " + methodname + " " + args + "\n";
+    }
 }
 
 std::string HelpExampleRpc(const std::string &methodname,
