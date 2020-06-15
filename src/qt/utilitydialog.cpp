f5c49684a09b3e5709355a37393ce99ddf37503a
Fabien <fabcien@gmail.com>
2020-06-15 14:11:49 +0200

    [Qt] Remove unused WindowFlags parameters
    Summary:
    ---
    
    The WindoFlags parameter is never set in the codebase, yet it triggers
    deprecation warnings with Qt 5.15.
    
    Test Plan:
    ---
    
    With Qt 5.5.1 and Qt 5.15:
    
        ninja all check
    
    - Observe that when compiling with Qt 5.15, there should be no more deprecation warnings.
    - Observe that when compiling with Qt 5.5.1, everything compiles.
    - Bonus: Run the app built under both Qt versions.
    
    ---
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D6572

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -146,8 +146,7 @@ void HelpMessageDialog::on_okButton_accepted() {
 }
 
 /** "Shutdown" window */
-ShutdownWindow::ShutdownWindow(QWidget *parent, Qt::WindowFlags f)
-    : QWidget(parent, f) {
+ShutdownWindow::ShutdownWindow(QWidget *parent) : QWidget(parent) {
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget(new QLabel(
         tr("%1 is shutting down...").arg(tr(PACKAGE_NAME)) + "<br /><br />" +
