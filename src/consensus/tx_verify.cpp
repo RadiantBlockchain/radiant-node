eb4b91b2ccc670bfc409169222dc9f604d0a366e
Amaury Séchet <deadalnix@gmail.com>
2020-06-23 16:20:14 +0200

    Remove double if in tx_verify.cpp
    Summary: And a handful of nits for casts.
    
    Test Plan:
      ninja all check-all
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D6682

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -128,7 +128,7 @@ std::pair<int, int64_t> CalculateSequenceLocks(const CTransaction &tx,
             nMinHeight = std::max(
                 nMinHeight,
                 nCoinHeight +
-                    (int)(txin.nSequence & CTxIn::SEQUENCE_LOCKTIME_MASK) - 1);
+                    int(txin.nSequence & CTxIn::SEQUENCE_LOCKTIME_MASK) - 1);
         }
     }
 
@@ -204,14 +204,12 @@ bool CheckTxInputs(const CTransaction &tx, CValidationState &state,
         assert(!coin.IsSpent());
 
         // If prev is coinbase, check that it's matured
-        if (coin.IsCoinBase()) {
-            if (nSpendHeight - coin.GetHeight() < COINBASE_MATURITY) {
-                return state.Invalid(
-                    false, REJECT_INVALID,
-                    "bad-txns-premature-spend-of-coinbase",
-                    strprintf("tried to spend coinbase at depth %d",
-                              nSpendHeight - coin.GetHeight()));
-            }
+        if (coin.IsCoinBase() &&
+            nSpendHeight - coin.GetHeight() < COINBASE_MATURITY) {
+            return state.Invalid(
+                false, REJECT_INVALID, "bad-txns-premature-spend-of-coinbase",
+                strprintf("tried to spend coinbase at depth %d",
+                          nSpendHeight - coin.GetHeight()));
         }
 
         // Check for negative or overflow input values
