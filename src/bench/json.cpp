73b549f845a508ce2e588ec25cec7015a79edb5d
BigBlockIfTrue <info@bitcoinlogo.eu>
2020-07-25 23:45:34 +0200

    Split JSON/RPC benchmarks for more targeted measurements
    The following changes measure individual runtime components more cleanly
    (which should help #51):
    
    * JSON I/O test has been split into reading, writing, and pretty writing
    (spoiler: writing turns out to be much more efficient than reading the
    same data)
    * JSON writing has been removed from block RPC test
    * Both RPC tests are now named consistently
    * Benchmark iteration counts have been recalibrated (2 seconds on
    BigBlockIfTrue computer should rougly approximate 1 second in real life)
    
    Test plan: `src/bench/bench_bitcoin -evals=1`

---
+++ src/bench/json.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2016-2019 The Bitcoin Core developers
+// Copyright (c) 2020 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <bench/data.h>
+
+#include <chain.h>
+#include <chainparams.h>
+#include <validation.h>
+#include <streams.h>
+#include <consensus/validation.h>
+#include <rpc/blockchain.h>
+#include <rpc/protocol.h>
+
+#include <univalue.h>
+
+static void JSONReadWriteBlock(const std::vector<uint8_t> &data, unsigned int pretty, bool write, benchmark::State &state) {
+    SelectParams(CBaseChainParams::MAIN);
+
+    CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
+    char a = '\0';
+    stream.write(&a, 1); // Prevent compaction
+
+    CBlock block;
+    stream >> block;
+
+    CBlockIndex blockindex;
+    const auto blockHash = block.GetHash();
+    blockindex.phashBlock = &blockHash;
+    blockindex.nBits = block.nBits;
+    const auto blockuv = blockToJSON(block, &blockindex, &blockindex, /*verbose*/ true);
+
+    if (write) {
+        while (state.KeepRunning()) {
+            (void)blockuv.write(pretty);
+        }
+    } else {
+        std::string json = blockuv.write(pretty);
+        while (state.KeepRunning()) {
+            UniValue uv;
+            if (!uv.read(json))
+                throw std::runtime_error("UniValue lib failed to parse its own generated string.");
+        }
+    }
+}
+
+static void JSONReadBlock_1MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block413567, 0, false, state);
+}
+static void JSONReadBlock_32MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block556034, 0, false, state);
+}
+static void JSONWriteBlock_1MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block413567, 0, true, state);
+}
+static void JSONWriteBlock_32MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block556034, 0, true, state);
+}
+static void JSONWritePrettyBlock_1MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block413567, 4, true, state);
+}
+static void JSONWritePrettyBlock_32MB(benchmark::State &state) {
+    JSONReadWriteBlock(benchmark::data::block556034, 4, true, state);
+}
+
+BENCHMARK(JSONReadBlock_1MB, 18);
+BENCHMARK(JSONReadBlock_32MB, 1);
+BENCHMARK(JSONWriteBlock_1MB, 52);
+BENCHMARK(JSONWriteBlock_32MB, 1);
+BENCHMARK(JSONWritePrettyBlock_1MB, 47);
+BENCHMARK(JSONWritePrettyBlock_32MB, 1);
--- src/bench/rpc_blockchain.cpp
+++ src/bench/rpc_blockchain.cpp
@@ -12,11 +12,10 @@
 #include <streams.h>
 #include <consensus/validation.h>
 #include <rpc/blockchain.h>
-#include <rpc/protocol.h>
 
 #include <univalue.h>
 
-static void BlockToJsonVerbose(const std::vector<uint8_t> &data, benchmark::State &state) {
+static void RPCBlockVerbose(const std::vector<uint8_t> &data, benchmark::State &state) {
     SelectParams(CBaseChainParams::MAIN);
 
     CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
@@ -32,51 +31,16 @@ static void BlockToJsonVerbose(const std::vector<uint8_t> &data, benchmark::Stat
     blockindex.nBits = block.nBits;
 
     while (state.KeepRunning()) {
-        (void)JSONRPCReply(
-            blockToJSON(block, &blockindex, &blockindex, /*verbose*/ true),
-            UniValue(UniValue::VNULL),
-            UniValue("benchmark"));
+        (void)blockToJSON(block, &blockindex, &blockindex, /*verbose*/ true);
     }
 }
 
-static void JsonReadWriteBlock(const std::vector<uint8_t> &data, benchmark::State &state) {
-    SelectParams(CBaseChainParams::MAIN);
-
-    CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
-    char a = '\0';
-    stream.write(&a, 1); // Prevent compaction
-
-    CBlock block;
-    stream >> block;
-
-    CBlockIndex blockindex;
-    const auto blockHash = block.GetHash();
-    blockindex.phashBlock = &blockHash;
-    blockindex.nBits = block.nBits;
-    const auto blockuv = blockToJSON(block, &blockindex, &blockindex, /*verbose*/ true);
-
-    UniValue uv;
-    while (state.KeepRunning()) {
-        // write it out to str and read it back again to test reading & writing of UniValue
-        if (!uv.read(blockuv.write(4/* pretty indent 4 spaces */)))
-            throw std::runtime_error("UniValue lib failed to parse its own generated string.");
-    }
+static void RPCBlockVerbose_1MB(benchmark::State &state) {
+    RPCBlockVerbose(benchmark::data::block413567, state);
+}
+static void RPCBlockVerbose_32MB(benchmark::State &state) {
+    RPCBlockVerbose(benchmark::data::block556034, state);
 }
 
-static void BlockToJsonVerbose_1MB(benchmark::State &state) {
-    BlockToJsonVerbose(benchmark::data::block413567, state);
-}
-static void BlockToJsonVerbose_32MB(benchmark::State &state) {
-    BlockToJsonVerbose(benchmark::data::block556034, state);
-}
-static void JsonReadWriteBlock_1MB(benchmark::State &state) {
-    JsonReadWriteBlock(benchmark::data::block413567, state);
-}
-static void JsonReadWriteBlock_32MB(benchmark::State &state) {
-    JsonReadWriteBlock(benchmark::data::block556034, state);
-}
-
-BENCHMARK(BlockToJsonVerbose_1MB, 10);
-BENCHMARK(BlockToJsonVerbose_32MB, 1);
-BENCHMARK(JsonReadWriteBlock_1MB, 17);
-BENCHMARK(JsonReadWriteBlock_32MB, 1);
+BENCHMARK(RPCBlockVerbose_1MB, 23);
+BENCHMARK(RPCBlockVerbose_32MB, 1);
