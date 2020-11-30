4d6467ce8bc1f4b9124b3b2d997cd678bbe254e7
freetrader <freetrader@tuta.io>
2020-11-30 23:46:29 +0000

    Update default assume valid and minimum chain work params after Axion activation
    These have been updated according to contrib/devtools/chainparams/README.md.
    
    The Python scripts to extract the data and generate the header have been
    updated to add testnet4 and scalenet.
    
    The assumed valid block for scalenet is chosen as 9,999 - just below the
    planned re-org height of 10,000.
    
    For all other test networks, the assumed valid block is still 2,000
    below the chain tip at the time of running the make_chainparams script.
    
    Test plan:
    - review (confirm assume hashes using block explorer)
    - `ninja check`
    
    Note: mainnet block 663078

--- contrib/devtools/chainparams/make_chainparams.py
+++ contrib/devtools/chainparams/make_chainparams.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2019 The Bitcoin developers
+# Copyright (c) 2019-2020 The Bitcoin developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -16,6 +16,8 @@ from authproxy import AuthServiceProxy  # noqa: E402
 class Chain(Enum):
     MainNet = "MAINNET"
     TestNet = "TESTNET"
+    TestNet4 = "TESTNET4"
+    ScaleNet = "SCALENET"
 
 
 def get_chainparams(rpc_caller, block):
@@ -23,8 +25,14 @@ def get_chainparams(rpc_caller, block):
     chaininfo = rpc_caller.getblockchaininfo()
     if chaininfo['chain'] == 'main':
         chain = Chain.MainNet
-    else:
+    elif chaininfo['chain'] == 'test':
         chain = Chain.TestNet
+    elif chaininfo['chain'] == 'test4':
+        chain = Chain.TestNet4
+    elif chaininfo['chain'] == 'scale':
+        chain = Chain.ScaleNet
+    else:
+        raise NotImplementedError
 
     # Use highest valid chainwork. This doesn't need to match the block hash
     # used by assume valid.
@@ -37,6 +45,9 @@ def get_chainparams(rpc_caller, block):
         block = chaininfo['blocks']
         if chain == Chain.MainNet:
             block -= 10
+        elif chain == Chain.ScaleNet:
+            # Use fixed block at height=9999 for scalenet because it re-orgs at 10,000
+            block = 9999
         else:
             block -= 2000
 
@@ -68,12 +79,13 @@ if __name__ == "__main__":
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--address', '-a', default="127.0.0.1:8332",
                         help="Node address for making RPC calls.\n"
-                             "The chain (MainNet or TestNet) will be automatically detected.\n"
+                             "The chain (MainNet, TestNet, TestNet4, ScaleNet) will be automatically detected.\n"
                              "Default: '127.0.0.1:8332'")
     parser.add_argument('--block', '-b',
                         help="The block hash or height to use for fetching chainparams.\n"
                              "MainNet default: 10 blocks from the chain tip."
-                             "TestNet default: 2000 blocks from the chain tip.")
+                             "ScaleNet default: block 9999 (fixed)"
+                             "All other test networks default: 2000 blocks from the chain tip.")
     parser.add_argument('--config', '-c', default="~/.bitcoin/bitcoin.conf",
                         help="Path to bitcoin.conf for RPC authentication arguments (rpcuser & rpcpassword).\n"
                              "Default: ~/.bitcoin/bitcoin.conf")
