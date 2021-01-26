8f6b5f71d7df8e67e6c19f22b009886fcabeb423
Calin Culianu <calin.culianu@gmail.com>
2021-01-26 14:27:09 +0200

    Seeder: Add Flowee's testnet4 seeder
    Summary
    ---
    
    The more seeders the better, just in case. This seeder is part of the
    Flowee intfrastructure so hopefully it will be up for some time.
    
    Test Plan
    ---
    
    1. `ninja all check`
    2. Make sure "testnet4-seed.flowee.cash" returns dns results:
      - `dig testnet4-seed.flowee.cash` - You should see a random assortment
        of IPs as A records.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -551,6 +551,8 @@ public:
         vSeeds.emplace_back("testnet4-seed-bch.toom.im");
         // Loping.net
         vSeeds.emplace_back("seed.tbch4.loping.net");
+        // Flowee
+        vSeeds.emplace_back("testnet4-seed.flowee.cash");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<uint8_t>(1, 111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<uint8_t>(1, 196);
