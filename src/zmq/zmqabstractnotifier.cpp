01f5955382af8ba73adaa145085c94b6470bba89
Daniel Kraft <d@domob.eu>
2021-02-03 00:19:09 +0200

    [backport] ZMQ: Small cleanups in the ZMQ code
    Co-authored-by: Calin Culianu <calin.culianu@gmail.com>
    
    Summary
    ---
    
    This is a backport of [Core#13686](https://github.com/bitcoin/bitcoin/pull/13686).
    
    While working on !1045 I noticed some un-idiomatic C++ usage and a
    potential memory leak. I went and looked at the Core repo and it turns
    out they have since updated the ZMQ subsystem to be better and they
    fixed the leak.
    
    Original commit message:
    
        This contains various small code cleanups that make the ZMQ code easier
        to read  and maintain (at least in my opinion). The only functional change
        is that a potential memory leak is fixed that would have occured when a
        notifier is removed from the notifiers list after its callback function
        returned false (which is likely not relevant in practice but still a bug).
    
    Note that this depends on MR !1045 (it is a commit on top of it).
    
    I adapted this code a little bit to alphabetize the headers and also
    other minor tiny nits. It's 99% the Core commit, essentially.
    
    Test Plan
    ---
    
    - `ninja all check`
    - `test/functional/test_runner.py interface_zmq`
    - `test/functional/test_runner.py rpc_zmq`

--- src/zmq/zmqabstractnotifier.cpp
+++ src/zmq/zmqabstractnotifier.cpp
@@ -1,10 +1,12 @@
 // Copyright (c) 2015 The Bitcoin Core developers
+// Copyright (c) 2021 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <util/system.h>
 #include <zmq/zmqabstractnotifier.h>
 
-#include <util/system.h>
+#include <cassert>
 
 CZMQAbstractNotifier::~CZMQAbstractNotifier() {
     assert(!psocket);
