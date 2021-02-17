78f9989a549374fe878e6a4068f0b10f9ced8038
BigBlockIfTrue <info@bitcoinlogo.eu>
2021-02-17 02:26:27 +0100

    Create ninja target for building Markdown documentation
    This moves auto-generation of Markdown documentation out of
    `contrib/devtools/gen-manpages.sh` and into the build system, with a new
    ninja target `doc-md`. `contrib/devtools/gen-manpages.sh` returns to its
    original purpose of generating manpages only (can be integrated into the
    build system later as well).
    
    The auto-generated Markdown documentation is no longer committed into
    the repository, but built as needed. CI will build it for GitLab pages,
    so that docs.bitcoincashnode.org will always be fully up-to-date with
    the master branch. A `DOC_ONLINE` option has been added to the build
    system to differentiate between documentation generated for the website
    and for local use (default off), replacing the previous CI hacks.
    
    Test plan: `ninja doc-html` (with `-DDOC_ONLINE=ON` and
    `-DDOC_ONLINE=OFF`),
    https://bigblockiftrue.gitlab.io/bitcoin-cash-node/

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -36,100 +36,13 @@ echo "[COPYRIGHT]" > footer.h2m
 
 for cmd in "$BITCOIND" "$BITCOINQT"; do
   cmdname="${cmd##*/}"
-  "$TOPDIR/contrib/devtools/cli-help-to-markdown.py" "$($cmd -?? -lang=en_US)" > "$TOPDIR/doc/cli/$cmdname.md"
-  sed -i "s/\-${BTCVER[1]}\(\-dirty\)\?//g" "$TOPDIR/doc/cli/$cmdname.md"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o "$TOPDIR/doc/man/$cmdname.1" --help-option="-? -lang=en_US" --version-option="-version -lang=en_US" "$cmd"
   sed -i "s/\\\-${BTCVER[1]}\(\\\-dirty\)\?//g" "$TOPDIR/doc/man/$cmdname.1"
 done
 
 for cmd in "$BITCOINCLI" "$BITCOINTX" "$BITCOINSEEDER"; do
   cmdname="${cmd##*/}"
-  "$TOPDIR/contrib/devtools/cli-help-to-markdown.py" "$($cmd -?)" > "$TOPDIR/doc/cli/$cmdname.md"
-  sed -i "s/\-${BTCVER[1]}\(\-dirty\)\?//g" "$TOPDIR/doc/cli/$cmdname.md"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o "$TOPDIR/doc/man/$cmdname.1" "$cmd"
   sed -i "s/\\\-${BTCVER[1]}\(\\\-dirty\)\?//g" "$TOPDIR/doc/man/$cmdname.1"
 done
 
-# Generate JSON-RPC API documentation.
-# Start a regtest node in a temporary data directory.
-mkdir -p "$TOPDIR/doc/json-rpc/tmp"
-"$BITCOIND" -daemon -debuglogfile=0 -regtest -datadir="$TOPDIR/doc/json-rpc/tmp" -rpcuser=gen-manpages -rpcpassword=gen-manpages
-# Create temporary new mkdocs file.
-rm -f "$TOPDIR/doc/mkdocs-tmp.yml"
-# Remove any existing JSON-RPC documentation.
-rm "$TOPDIR"/doc/json-rpc/*.md
-# Get daemon version which will be included in footer.
-version="$($BITCOIND -version | head -n1)"
-# Iterate over the existing mkdocs file and locate the entry for json-rpc/README.md.
-indentation=""
-while IFS= read -r line; do
-  if [ "${line: -23}" == " doc/json-rpc/README.md" ]; then
-    # json-rpc/README.md found; preserve it.
-    echo "$line" >> "$TOPDIR/doc/mkdocs-tmp.yml"
-    indentation="${line%%-*}"
-    # The list of RPC commands will be inserted into the new mkdocs file below the readme entry.
-    # Get the list of RPC commands from the node and process it.
-    echo "Bitcoin Cash Node JSON-RPC commands" > "$TOPDIR/doc/json-rpc/README.md"
-    echo "===================================" >> "$TOPDIR/doc/json-rpc/README.md"
-    "$BITCOINCLI" -rpcwait -regtest -datadir="$TOPDIR/doc/json-rpc/tmp" -rpcuser=gen-manpages -rpcpassword=gen-manpages help | while read -r helpline; do
-      if [ -n "$helpline" ]; then
-        if [ "${helpline:0:3}" == "== " ] && [ "${helpline: -3}" == " ==" ]; then
-          # Found a category.
-          category="${helpline:3:-3}"
-          # Write category to new mkdocs file.
-          echo "$indentation- $category:" >> "$TOPDIR/doc/mkdocs-tmp.yml"
-          # Write category to readme file.
-          {
-              echo
-              echo "## $category"
-              echo
-          } >> "$TOPDIR/doc/json-rpc/README.md"
-        else
-          # Found a command.
-          command=${helpline%% *}
-          # Write command to new mkdocs file.
-          echo "$indentation    - $command: doc/json-rpc/$command.md" >> "$TOPDIR/doc/mkdocs-tmp.yml"
-          # Create command help page.
-          "$TOPDIR/contrib/devtools/rpc-help-to-markdown.py" "$($BITCOINCLI -rpcwait -regtest -datadir="$TOPDIR/doc/json-rpc/tmp" -rpcuser=gen-manpages -rpcpassword=gen-manpages help $command)" > "$TOPDIR/doc/json-rpc/$command.md"
-          {
-              echo
-              echo "***"
-              echo
-              echo "*$version*"
-          } >> "$TOPDIR/doc/json-rpc/$command.md"
-          sed -i "s/\-${BTCVER[1]}\(\-dirty\)\?//g" "$TOPDIR/doc/json-rpc/$command.md"
-          # Write command to readme file.
-          if [ "$command" == "$helpline" ]; then
-            echo "* [**\`$command\`**]($command.md)" >> "$TOPDIR/doc/json-rpc/README.md"
-          else
-            echo "* [**\`$command\`**\` ${helpline:${#command}}\`]($command.md)" >> "$TOPDIR/doc/json-rpc/README.md"
-          fi
-        fi
-      fi
-    done
-    {
-        echo
-        echo "***"
-        echo
-        echo "*$version*"
-    } >> "$TOPDIR/doc/json-rpc/README.md"
-    sed -i "s/\-${BTCVER[1]}\(\-dirty\)\?//g" "$TOPDIR/doc/json-rpc/README.md"
-  else
-    # Detect the end of indentation below the readme entry.
-    if [ "${line:0:${#indentation}}" != "$indentation" ]; then
-      indentation=""
-    fi
-    # Copy existing mkdocs entries into the new mkdocs file, but
-    # skip all existing entries below the readme entry on the same indentation level.
-    # This removes previously generated RPC documentation.
-    if [ "$indentation" == "" ]; then
-      echo "$line" >> "$TOPDIR/doc/mkdocs-tmp.yml"
-    fi
-  fi
-done < "$TOPDIR/doc/mkdocs.yml"
-# Stop the regtest node
-"$BITCOINCLI" -rpcwait -regtest -datadir="$TOPDIR/doc/json-rpc/tmp" -rpcuser=gen-manpages -rpcpassword=gen-manpages stop
-# Replace the old mkdocs file with the new one.
-mv -f "$TOPDIR/doc/mkdocs-tmp.yml" "$TOPDIR/doc/mkdocs.yml"
-# Remove the temporary node data directory
-rm -r "$TOPDIR/doc/json-rpc/tmp"
