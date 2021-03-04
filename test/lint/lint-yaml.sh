1a0ac9a7daf633f4f0404cce1cb605d97ba8da5d
exteradox <exteradox@pm.me>
2021-03-04 07:39:23 +0000

    Add 'lint-yaml' linter. Fix lint errors in existing yaml files.

---
+++ test/lint/lint-yaml.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#
+# Lint YAML files.
+
+SCRIPT_DIR="$(dirname "$0")"
+LINT_CONFIG="$SCRIPT_DIR/lint-yaml-config.yml"
+
+if ! command -v yamllint &>/dev/null; then
+  echo "WARNING: yamllint not found. Skipping lint-yaml."
+  exit 0
+fi
+
+EXIT_CODE=0
+
+# Filter out yaml files belonging to upstream projects.
+FILES=$(
+  git ls-files '*.yml' '*.yaml' '*.yml.in' '*.yaml.in' | \
+  grep -v -e contrib/ -e src/secp256k1/ -e src/leveldb/
+)
+for filename in $FILES; do
+  echo "Linting $filename"
+  if ! yamllint -c "$LINT_CONFIG" -- "$filename"; then
+    EXIT_CODE=1
+  fi
+done
+
+exit $EXIT_CODE
