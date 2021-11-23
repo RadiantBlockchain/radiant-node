f9417f4120c3e3da4938d258d4e9e933b959d0a8
freetrader <freetrader@tuta.io>
2021-11-23 17:34:28 +0000

    [qa] Move activation tests upgrade checks from Axion to upgrade8 (May 2022)
    Mainly functional test changes. Passes the upgrade 8 activation time instead of the Axion 
    activation time,  implementing necessary parameter bits in the test framework where 
    necessary..
    
    Test plan:
    
    * `ninja all bench_bitcoin check check-functional-extended check-upgrade-activated-extended`

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -43,6 +43,7 @@ struct Params {
     int phononHeight;
     /** Unix time used for MTP activation of 15 Nov 2020 12:00:00 UTC upgrade */
     int axionActivationTime;
+
     /** Note: Unix time used for MTP activation of the 15 May 2021 12:00:00 UTC upgrade was 1621080000, but since
      *  it was a relay-rules-only upgrade, so we no longer track this time for blockchain consensus. */
     /** Unix time used for MTP activation of 15 May 2022 12:00:00 UTC upgrade */
--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -130,8 +130,8 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
                             help="Attach a python debugger if test fails")
         parser.add_argument("--usecli", dest="usecli", default=False, action="store_true",
                             help="use bitcoin-cli instead of RPC for all commands")
-        parser.add_argument("--with-axionactivation", dest="axionactivation", default=False, action="store_true",
-                            help="Activate axion update on timestamp {}".format(TIMESTAMP_IN_THE_PAST))
+        parser.add_argument("--with-upgrade8activation", dest="upgrade8activation", default=False, action="store_true",
+                            help="Activate May 2022 (upgrade 8) update on timestamp {}".format(TIMESTAMP_IN_THE_PAST))
         parser.add_argument("--extra-bitcoind-args", dest="extra_bitcoind_args", default="",
                             help="Start bitcoind with these additional arguments (comma separated)")
         self.add_options(parser)
@@ -334,9 +334,9 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
                 use_cli=self.options.usecli,
                 emulator=self.options.emulator,
             ))
-            if self.options.axionactivation:
+            if self.options.upgrade8activation:
                 self.nodes[i].extend_default_args(
-                    ["-axionactivationtime={}".format(TIMESTAMP_IN_THE_PAST)])
+                    ["-upgrade8activationtime={}".format(TIMESTAMP_IN_THE_PAST)])
             if len(self.options.extra_bitcoind_args):
                 self.nodes[i].extend_default_args(
                         self.options.extra_bitcoind_args.split(","))
@@ -501,9 +501,9 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
                 if i > 0:
                     self.nodes[i].extend_default_args(
                         ["-connect=127.0.0.1:" + str(p2p_port(0))])
-                if self.options.axionactivation:
+                if self.options.upgrade8activation:
                     self.nodes[i].extend_default_args(
-                        ["-axionactivationtime={}".format(TIMESTAMP_IN_THE_PAST)])
+                        ["-upgrade8activationtime={}".format(TIMESTAMP_IN_THE_PAST)])
                 self.start_node(i)
 
             # Wait for RPC connections to be ready
