127c9d4422c4ddf809e1f3c77ddb724c8799fabc
SlowRiot <slowriot@voxelstorm.com>
2021-12-09 22:55:26 +0000

    Add linting of include header guards
    This backports the include header guard linter from bitcoin core, and adds it to the cmake lint suite. Addresses #257.
    
    Additionally, allows the linter to pass if `#pragma once` is present in the file, paving the way for future replacement of header guards with the pragma.
    
    Test plan:
    
    - Modify one or more header files to disturb the header guards (e.g. remove them) and check that `ninja check-lint` correctly reports the violation.
    - Remove header guards on a file and replace by `#pragma once` and check that the header is deemed OK by the linter

---
+++ test/lint/lint-include-guards.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018-2020 The Bitcoin Core developers
+# Copyright (c) 2021 The Bitcoin Cash Node developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#
+# Check include guards.
+
+export LC_ALL=C
+HEADER_ID_PREFIX="BITCOIN_"
+HEADER_ID_SUFFIX="_H"
+
+REGEXP_EXCLUDE_FILES_WITH_PREFIX="src/(crypto/ctaes/|leveldb/|secp256k1/|tinyformat.h|univalue/)"
+
+EXIT_CODE=0
+for HEADER_FILE in $(git ls-files -- "*.h" | grep -vE "^${REGEXP_EXCLUDE_FILES_WITH_PREFIX}")
+do
+    # check for "#pragma once" - if present, don't verify include guard format - but only if preceded by whitespace, i.e. commented pragmas fail the test
+    if grep -q '^[[:space:]]*#pragma once' "${HEADER_FILE}"; then
+        continue
+    fi
+
+    HEADER_ID_BASE=$(cut -f2- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr - _ | tr "[:lower:]" "[:upper:]")
+    HEADER_ID="${HEADER_ID_PREFIX}${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
+    if [[ $(grep --count --extended-regexp "^#(ifndef|define|endif //) ${HEADER_ID}" "${HEADER_FILE}") != 3 ]]; then
+        echo "${HEADER_FILE} seems to be missing the expected include guard:"
+        echo "  #ifndef ${HEADER_ID}"
+        echo "  #define ${HEADER_ID}"
+        echo "  ..."
+        echo "  #endif // ${HEADER_ID}"
+        echo
+        EXIT_CODE=1
+    fi
+done
+exit ${EXIT_CODE}
