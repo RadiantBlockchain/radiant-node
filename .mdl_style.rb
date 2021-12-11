f5d7b0adccb6ad1ba49c6bbe09eb6e5091b2df6e
SlowRiot <slowriot@voxelstorm.com>
2021-12-11 15:39:00 +0000

    Markdown code quality, 1 of 5 (enable markdownlint in codeclimate config)
    One of a multi-part series of changes to improve markdown code quality,
    primarily driven by feedback from codeclimate's markdownlint plugin.
    This first change enables the markdownlint plugin and configures the rules,
    so subsequent changes will show the improvements in code quality metrics
    within gitlab.

---
+++ .mdl_style.rb
@@ -0,0 +1,20 @@
+# Markdownlint style definition - see .mdlrc
+all
+#exclude-tag :line_length
+
+# Header style
+#rule 'MD003', :style => 'consistent'
+
+# line length:
+rule 'MD013', :line_length => 300
+
+# multiple headers with the same content
+#rule 'MD024', :allow_different_nesting => true
+exclude_rule 'MD024'
+
+# fenced code blocks should have a language specified:
+exclude_rule 'MD040'
+
+# code block style:
+#rule 'MD046', :style => 'consistent'
+exclude_rule 'MD046'
