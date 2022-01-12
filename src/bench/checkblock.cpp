02e12e0dfcbe312b06fc037c8442807aa714a562
SlowRiot <slowriot@voxelstorm.com>
2022-01-12 10:15:11 +0000

    Add benchmarks for CheckBlock, CheckProofOfWork and GetHash
    Benchmarks already exist for DeserializeAndCheckBlock, but there is currently
    no way to precisely measure the performance of the CheckBlock,
    CheckProofOfWork and GetHash functions decoupled from deserialisation.
    
    This MR adds two such benchmarks, and runs them for both 1MB and 32MB blocks
    (although there should be little difference between these for PoW as it just
    checks the hash, it can be useful for identifying future regressions).
    This is a useful precursor to any optimisation attempts around block validation.
    
    Test Plan
    ---------
    
    ```
    ninja bench_bitcoin`
    src/bench/bench_bitcoin -filter='CheckBlock.*'
    src/bench/bench_bitcoin -filter='CheckProofOfWork.*'
    ```

--- src/bench/checkblock.cpp
+++ src/bench/checkblock.cpp
@@ -8,6 +8,7 @@
 #include <chainparams.h>
 #include <config.h>
 #include <consensus/validation.h>
+#include <pow.h>
 #include <streams.h>
 #include <validation.h>
 
@@ -50,6 +51,60 @@ static void DeserializeAndCheckBlockTest(const std::vector<uint8_t> &data, bench
     }
 }
 
+static void CheckBlockTest(const std::vector<uint8_t> &data, benchmark::State &state) {
+    CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
+    char a = '\0';
+    stream.write(&a, 1); // Prevent compaction
+
+    const Config &config = GetConfig();
+    const Consensus::Params params = config.GetChainParams().GetConsensus();
+    BlockValidationOptions options(config);
+    CBlock block;
+    stream >> block;
+    CValidationState validationState;
+
+    // de-serialize once, check many times
+    while (state.KeepRunning()) {
+        block.fChecked = false; // reset block checked state
+
+        bool checked = CheckBlock(block, validationState, params, options);
+        assert(checked);
+    }
+}
+
+static void CheckProofOfWorkTest(const std::vector<uint8_t> &data, benchmark::State &state) {
+    CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
+    char a = '\0';
+    stream.write(&a, 1); // Prevent compaction
+
+    const Config &config = GetConfig();
+    const Consensus::Params params = config.GetChainParams().GetConsensus();
+    CBlock block;
+    stream >> block;
+
+    // de-serialize once, check many times
+    while (state.KeepRunning()) {
+        bool checked = CheckProofOfWork(block.GetHash(), block.nBits, params);
+        assert(checked);
+    }
+}
+
+static void CheckBlockHashTest(const std::vector<uint8_t> &data, benchmark::State &state, std::string const &expected_hash) {
+    CDataStream stream(data, SER_NETWORK, PROTOCOL_VERSION);
+    char a = '\0';
+    stream.write(&a, 1); // Prevent compaction
+
+    CBlock block;
+    stream >> block;
+
+    BlockHash const expected = BlockHash::fromHex(expected_hash);
+
+    while (state.KeepRunning()) {
+        BlockHash hash = block.GetHash();
+        assert(hash == expected);
+    }
+}
+
 static void DeserializeBlockTest_1MB(benchmark::State &state) {
     DeserializeBlockTest(benchmark::data::Get_block413567(), state);
 }
@@ -62,8 +117,32 @@ static void DeserializeAndCheckBlockTest_1MB(benchmark::State &state) {
 static void DeserializeAndCheckBlockTest_32MB(benchmark::State &state) {
     DeserializeAndCheckBlockTest(benchmark::data::Get_block556034(), state);
 }
+static void CheckBlockTest_1MB(benchmark::State &state) {
+    CheckBlockTest(benchmark::data::Get_block413567(), state);
+}
+static void CheckBlockTest_32MB(benchmark::State &state) {
+    CheckBlockTest(benchmark::data::Get_block556034(), state);
+}
+static void CheckProofOfWorkTest_1MB(benchmark::State &state) {
+    CheckProofOfWorkTest(benchmark::data::Get_block413567(), state);
+}
+static void CheckProofOfWorkTest_32MB(benchmark::State &state) {
+    CheckProofOfWorkTest(benchmark::data::Get_block556034(), state);
+}
+static void CheckBlockHashTest_1MB(benchmark::State &state) {
+    CheckBlockHashTest(benchmark::data::Get_block413567(), state, "0000000000000000025aff8be8a55df8f89c77296db6198f272d6577325d4069");
+}
+static void CheckBlockHashTest_32MB(benchmark::State &state) {
+    CheckBlockHashTest(benchmark::data::Get_block556034(), state, "000000000000000000eb279368d5e158e5ef011010c98da89245f176e2083d64");
+}
 
 BENCHMARK(DeserializeBlockTest_1MB, 160);
 BENCHMARK(DeserializeBlockTest_32MB, 3);
 BENCHMARK(DeserializeAndCheckBlockTest_1MB, 130);
 BENCHMARK(DeserializeAndCheckBlockTest_32MB, 2);
+BENCHMARK(CheckBlockTest_1MB, 1600);
+BENCHMARK(CheckBlockTest_32MB, 20);
+BENCHMARK(CheckProofOfWorkTest_1MB, 1'000'000);
+BENCHMARK(CheckProofOfWorkTest_32MB, 1'000'000);
+BENCHMARK(CheckBlockHashTest_1MB, 1'000'000);
+BENCHMARK(CheckBlockHashTest_32MB, 1'000'000);
